bc79c3b0a40cc3aa1a322e7689e314b4d4897645
Madison Kawakami <madi.kawakami@gmail.com>
2017-03-06 08:43:28 -1000

    new

--- app.js
+++ app.js
@@ -4,22 +4,35 @@
 */
 function accessObjectInArray(arr){
 	//Print the parameter being passed through
+	console.log(arr);
 
 	//How can we tell what type of data this is?
 	//By it's notation, the top level is bracket notation, therefore the parameter is an array
 
 	//What methods can we use for this item?
 	//Array methods
+	console.log(arr.length);
 
 	//Access and print the first object in the parameter 
+	console.log(arr[0]);
 
 	//Access and print the third object in the parameter
+	console.log(arr[2]);
 
 	//Iterate through the array to print all the objects in the parameter
-
+	for (var i = 0; i < arr.length; i++){
+		// console.log(arr[i];)
 		//Can you concatanete a string to an object?
+		// no
+		// console.log("app.js, line 25");
+		console.log(arr[i];)
 
-		//Iterate through the key:value pairings and print all the keys and their values
+	//Iterate through the key:value pairings and print all the keys and their values
+	for (var key in arr){
+		// console.log(key);
+		console.log(arr[key]);
+		}
+	}
 
 	//Clean up your code by creating variable names and setting their values
 }
@@ -50,12 +63,33 @@ function accessArrayInObject(obj){
 	//Is the parameter an array or an object?
 
 	//Print the paramter being passed through the function
+	console.log(obj);
+
 
 	//Iterate through the object
+	for (var key in obj){
+		console.log("key");
+		console.log(key);
+		console.log("\nValues");
+		console.log(obj[key]);
+		console.log("\n");
+		//Create a conditional to check if the key is "cars"
+		if (key === "cars") {
+			console.log("Inside conditional for 'cars'")
+			console.log(obj[key]);
+			for (var carValues in obj[key]){
+				console.log("Values for keys in 'cars'");
+				console.log(obj[key][carValues]);
+			}
+		}
+	}
 
-	//Create a conditional to check if the key is "cars"
 
 	//Iterate through and print the values in the key "cars"
+
+
+
+
 
 	//Iterate through and print the key value pairs for each object in the array "toppings"
 
